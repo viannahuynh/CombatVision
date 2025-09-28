aaf4dc6e2a060fdf4cae61054bb491d6d7bbe3a5
Ellie Carron <elliecarron@gmail.com>
2025-09-28 04:02:38 -0400

    Update PlayPage.jsx

--- cv-app/src/pages/PlayPage.jsx
+++ cv-app/src/pages/PlayPage.jsx
@@ -1,3 +1,17 @@
+import lightsaber from './assets/lightsaber.gif'
+import lightsaber2 from './assets/lightsaber2.gif'
+import redlightsaber from './assets/redlightsaber.gif'
+import sailormoonwand from './assets/sailormoonwand.gif'
+import fightsong from './assets/fightsong.mp3'
+import koSound from './assets/ko.mp3'
+import HealthBarSprites from './components/HealthBar'
+import {
+  drawHands, centerFromPose, estimateBoxSize, Ema2D,
+  snapBox, drawBoxOutline, assignLeftRight,
+  handsToPixel, palmCenter, splitHandsByPlayer, pickMostExtendedHand,
+  lineIntersectsRect, makeImage, drawCenteredImage
+} from './cv/helpers'
+
 export default function PlayPage() {
   return (
     <div style={{ minHeight:"100vh", display:"grid", placeItems:"center" }}>
